51b8f05c8f5b05ae170bdaec5835ac6a4511dc3b
Gabr19 <sannagabriele19@gmail.com>
2022-12-21 19:54:22 +0100

    aggiunta if

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,35 @@ buttonsubmit.addEventListener('click' ,
 
     function() {
 
-        console.log('kminput.value' , kminput.value)
-        console.log('ageinput' , ageinput.value)
+        // console.log('kminput.value' , kminput.value)
+        // console.log('ageinput' , ageinput.value)
+
+        if (ageinput.value>=19 && ageinput<=64) {
+            let x = kminput;
+            let y = 0.21;
+            let z = x * y;
+            console.log( z.toFixed( 2 ) );
+            // document.getElementById("number").innerHTML=( z.toFixed( 2 ) ) ;
         
+        
+        }
+        
+        else if (ageinput>=65) {
+            let x = kminput;
+            let y = 0.21;
+            let z = x * y * 0.6;
+            console.log( z.toFixed( 2 ) );
+            // document.getElementById("number").innerHTML=( z.toFixed( 2 ) ) ;
+        }
+        
+        else {
+            let x = kminput;
+            let y = 0.21;
+            let z = x * y * 0.8;
+            console.log( z.toFixed( 2 ) );
+            // document.getElementById("number").innerHTML=( z.toFixed( 2 ) ) ;
+        
+        }
 
     }
 )
